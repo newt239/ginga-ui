211c8fb7d0a561516b7801a0c9f727765de59062
newt <ysti2019.win2020.st@gmail.com>
2024-10-16 18:00:27 +0900

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest">
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { name } from "./package.json";
+
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: formattedName,
+      formats: ["es", "umd"],
+      fileName: (format) => `${formattedName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react/jsx-runtime": "react/jsx-runtime",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
