6f561b3643360c138d06fe68ad83e903964b7f94
newt <ysti2019.win2020.st@gmail.com>
2024-10-16 18:58:10 +0900

    fix: Checkbox style

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Button from "./Button/Button";
 import TextInput from "./TextInput/TextInput";
 import Switch from "./Switch/Switch";
+import Checkbox from "./Checkbox/Checkbox";
 
-export { Button, TextInput, Switch };
+export { Button, TextInput, Switch, Checkbox };
