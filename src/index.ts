43d652f64bc1f32088fba82612f964aca5f7d36b
newt <ysti2019.win2020.st@gmail.com>
2024-10-16 18:27:18 +0900

    fix: build settings

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import "./index.css";
+import "./styles/variables.css";
 export * from "./components/";
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { name } from "./package.json";
+import dts from "vite-plugin-dts";
 
 const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
