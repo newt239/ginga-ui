76fef4edbe8253a83e8ba248b8e2d8191e08d775
newt <ysti2019.win2020.st@gmail.com>
2024-10-18 16:04:14 +0900

    add: heading componet

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Heading } from "./Heading";
+
+const meta: Meta<typeof Heading> = {
+  component: Heading,
+};
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+export const FirstStory: Story = {
+  args: {
+    level: "h1",
+    children: "Heading 1",
+  },
+};
---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { cn } from "../../lib/utils";
+import classes from "./Heading.module.css";
+
+export type HeadingProps = React.HTMLAttributes<HTMLHeadingElement> & {
+  level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+};
+
+const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
+  ({ className, level, ...props }, ref) => {
+    switch (level) {
+      case "h1":
+        return (
+          <h1 className={cn(classes.h1, className)} ref={ref} {...props} />
+        );
+      case "h2":
+        return (
+          <h2 className={cn(classes.h2, className)} ref={ref} {...props} />
+        );
+      case "h3":
+        return (
+          <h3 className={cn(classes.h3, className)} ref={ref} {...props} />
+        );
+      case "h4":
+        return (
+          <h4 className={cn(classes.h4, className)} ref={ref} {...props} />
+        );
+      case "h5":
+        return (
+          <h5 className={cn(classes.h5, className)} ref={ref} {...props} />
+        );
+      case "h6":
+        return (
+          <h6 className={cn(classes.h6, className)} ref={ref} {...props} />
+        );
+      default:
+        return null;
+    }
+  }
+);
+Heading.displayName = "Heading";
+
+export { Heading };
