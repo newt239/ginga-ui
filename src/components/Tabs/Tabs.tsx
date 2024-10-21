053955b825f94db0aeabd16defde976064223e1e
newt <ysti2019.win2020.st@gmail.com>
2024-10-21 21:44:46 +0900

    feat: add Tabs component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,56 @@
+import React, { ComponentProps } from "react";
+import {
+  Tab as AriaTab,
+  TabList as AriaTabList,
+  TabPanel as AriaTabPanel,
+  Tabs as AriaTabs,
+} from "react-aria-components";
+
+import { cn } from "@/lib/utils";
+import classes from "./Tabs.module.css";
+
+export type TabProps = ComponentProps<typeof AriaTab>;
+
+const Tab: React.FC<TabProps> = ({ children, className, ...props }) => {
+  return (
+    <AriaTab className={cn(classes.Tab, className)} {...props}>
+      {children}
+    </AriaTab>
+  );
+};
+
+export type TabsProps = ComponentProps<typeof AriaTabs>;
+
+const Tabs: React.FC<TabsProps> = ({ children, className, ...props }) => {
+  return (
+    <AriaTabs className={cn(classes.Tabs, className)} {...props}>
+      {children}
+    </AriaTabs>
+  );
+};
+
+export type TabListProps = ComponentProps<typeof AriaTabList>;
+
+const TabList: React.FC<TabListProps> = ({ children, className, ...props }) => {
+  return (
+    <AriaTabList className={cn(classes.TabList, className)} {...props}>
+      {children}
+    </AriaTabList>
+  );
+};
+
+export type TabPanelProps = ComponentProps<typeof AriaTabPanel>;
+
+const TabPanel: React.FC<TabPanelProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <AriaTabPanel className={cn(classes.TabPanel, className)} {...props}>
+      {children}
+    </AriaTabPanel>
+  );
+};
+
+export { Tab, TabList, TabPanel, Tabs };
