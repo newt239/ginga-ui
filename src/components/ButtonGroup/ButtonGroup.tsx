fa20e0cb837eaea92090d876f200da7dcd33c23f
newt <ysti2019.win2020.st@gmail.com>
2024-10-24 21:46:09 +0900

    fix: ButtonGroup css

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,6 +1,7 @@
+import { type ButtonProps } from "@/components/Button/Button";
 import { cn } from "@/lib/utils";
 import React, { ComponentProps } from "react";
-import { type ButtonProps } from "../Button/Button";
+import classes from "./ButtonGroup.module.css";
 
 export type ButtonGroupProps = ComponentProps<"div"> & {
   children: React.ReactElement<ButtonProps>[];
@@ -12,12 +13,12 @@ export const ButtonGroup: React.FC<ButtonGroupProps> = ({
   ...props
 }) => {
   return (
-    <div className={cn("button-group", className)} {...props}>
+    <div className={cn(classes.ButtonGroup, className)} {...props}>
       {React.Children.map(children, (child, index) => {
         if (!React.isValidElement(child)) return null;
 
         return React.cloneElement<ButtonProps>(child, {
-          className: cn(child.props.className, "button-group__item"),
+          className: cn(classes.ButtonGroupItem, child.props.className),
           "data-position":
             index === 0
               ? "first"
