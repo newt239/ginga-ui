a1d108da3077402b6fc187fdbba90879fd1a5504
newt <ysti2019.win2020.st@gmail.com>
2024-11-13 19:54:27 +0900

    feat: add Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+import styles from "./Container.module.css";
+
+export type ContainerProps = React.HTMLAttributes<HTMLDivElement> & {
+  size?: "sm" | "md" | "lg";
+};
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  ({ className, size = "md", ...props }, ref) => {
+    return (
+      <div
+        className={cn(styles["container"], className)}
+        data-size={size}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+export default Container;
