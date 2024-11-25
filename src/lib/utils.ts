61196a8783855410a5ba4b9e59cd9f3d7836047f
newt <ysti2019.win2020.st@gmail.com>
2024-11-25 19:29:42 +0900

    fix: cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,14 @@
-export function cn(...inputs: string[]) {
-  return inputs.filter(Boolean).join(" ");
+type ClassName =
+  | string
+  | undefined
+  | null
+  | false
+  // eslint-disable-next-line no-unused-vars
+  | ((...args: any[]) => string);
+
+export function cn(...args: ClassName[]): string {
+  return args
+    .map((arg) => (typeof arg === "function" ? arg() : arg))
+    .filter(Boolean)
+    .join(" ");
 }
