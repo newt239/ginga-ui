82705b9e284c1def5e60f4ab7f8ab2242341471b
newt <ysti2019.win2020.st@gmail.com>
2024-12-25 17:15:25 +0900

    fix: type error of checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -6,23 +6,19 @@ import styles from "./Checkbox.module.css";
 
 import { cn } from "@/lib/utils";
 
-export type CheckboxProps = ComponentProps<typeof AriaCheckbox>;
+export type CheckboxProps = ComponentProps<typeof AriaCheckbox> & {
+  label: string;
+};
 
-const Checkbox: React.FC<CheckboxProps> = ({
-  children,
-  className,
-  ...props
-}) => {
+const Checkbox: React.FC<CheckboxProps> = ({ label, className, ...props }) => {
   return (
     <AriaCheckbox className={cn(styles["checkbox"], className)} {...props}>
-      <>
-        <div className={styles["checkbox-input"]}>
-          <svg viewBox="0 0 18 18" aria-hidden="true">
-            <polyline points="1 9 7 14 15 4" />
-          </svg>
-        </div>
-        {children}
-      </>
+      <div className={styles["checkbox-input"]}>
+        <svg viewBox="0 0 18 18" aria-hidden="true">
+          <polyline points="1 9 7 14 15 4" />
+        </svg>
+      </div>
+      {label}
     </AriaCheckbox>
   );
 };
