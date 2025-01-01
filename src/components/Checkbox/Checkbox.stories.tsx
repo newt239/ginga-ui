cc5eafc5be2884ef328771733e02a2b7ad153de3
newt <ysti2019.win2020.st@gmail.com>
2025-01-01 21:46:54 +0900

    fix: update Checkbox stories to use label prop instead of children

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -19,43 +19,45 @@ type Story = StoryObj<typeof Checkbox>;
 
 export const Default: Story = {
   args: {
-    children: "Default Checkbox",
+    label: "Default Checkbox",
   },
 };
 
 export const Selected: Story = {
   args: {
-    children: "Selected Checkbox",
+    label: "Selected Checkbox",
     isSelected: true,
   },
 };
 
 export const Disabled: Story = {
   args: {
-    children: "Disabled Checkbox",
+    label: "Disabled Checkbox",
     isDisabled: true,
   },
 };
 
 export const DisabledSelected: Story = {
   args: {
-    children: "Disabled Selected Checkbox",
+    label: "Disabled Selected Checkbox",
     isDisabled: true,
     isSelected: true,
   },
 };
 
 export const WithoutLabel: Story = {
-  args: {},
+  args: {
+    label: "",
+  },
 };
 
 // TODO: must specify the value prop for each Checkbox
 export const CheckboxGroupExample: Story = {
   render: () => (
     <CheckboxGroup>
-      <Checkbox value="one">Option 1</Checkbox>
-      <Checkbox value="two">Option 2</Checkbox>
-      <Checkbox value="three">Option 3</Checkbox>
+      <Checkbox value="one" label="Option 1" />
+      <Checkbox value="two" label="Option 2" />
+      <Checkbox value="three" label="Option 3" />
     </CheckboxGroup>
   ),
 };
