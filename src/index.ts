35f26229a6c34a215777a8bbb823a20f415c2aef
newt <ysti2019.win2020.st@gmail.com>
2025-01-09 20:20:40 +0900

    feat: import components from

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./components/";
 export { default as ThemeClient } from "./lib/ai/themeClient";
+export * from "./lib/color";
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     dts({
       insertTypesEntry: true,
-      outDir: "dist",
+      outDir: "dist/types",
     }),
   ],
   resolve: {
@@ -22,41 +22,29 @@ export default defineConfig({
   build: {
     target: "esnext",
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "ginga-ui",
-      fileName: (format) => `ginga-ui.${format}.js`,
+      entry: {
+        main: path.resolve(__dirname, "src/index.ts"),
+      },
       formats: ["es", "cjs"],
+      fileName: "[format]/[name]",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
-      output: [
-        {
-          format: "es",
-          dir: "dist",
-          entryFileNames: "ginga-ui.es.js",
-          banner: '"use client"',
+      input: {
+        components: "src/components/index.ts",
+        ai: "src/lib/ai/themeClient.ts",
+      },
+      output: {
+        preserveModules: true,
+        exports: "named",
+        banner: (chunk) => {
+          if (chunk.facadeModuleId?.includes("components")) {
+            return `"use client";`;
+          }
+          return undefined;
         },
-        {
-          format: "es",
-          preserveModules: true,
-          preserveModulesRoot: "src",
-          entryFileNames: "[name].es.js",
-          banner: '"use client"',
-        },
-        {
-          format: "cjs",
-          dir: "dist",
-          entryFileNames: "ginga-ui.cjs.js",
-          banner: '"use client"',
-        },
-        {
-          format: "cjs",
-          preserveModules: true,
-          preserveModulesRoot: "src",
-          entryFileNames: "[name].cjs.js",
-          banner: '"use client"',
-        },
-      ],
+        assetFileNames: "ginga-ui[extname]",
+      },
     },
   },
 });
